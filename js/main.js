87b314d584cc87b41c49feea55420937c14465d8
BlackSheepAgencyClem <hmich.c@gmail.com>
2015-01-27 13:07:02 +0100

    commit calendar mobile

--- js/main.js
+++ js/main.js
@@ -35,7 +35,6 @@ $(document).ready(function() {
 
 	}
 
-	
 
 	/*
 	*	HOME
@@ -196,6 +195,29 @@ $(document).ready(function() {
 				$(this).attr('data-id')
 			});
 		}
+	});
+
+
+	/*
+	*	CALENDRIER MOBILE
+	*/
+
+	var extended = 0;
+	var ratio = 1920/1080;
+
+	$('.info_btn_m').off('click');
+	$('.info_btn_m').on('click', function() {
+		if(extended == 0) {
+			$('.detail_m').slideDown();
+			extended = 1;
+		} else {
+			$('.detail_m').slideUp();
+			extended = 0;
+		}
+	});
+
+	$('.info_btn_m').click(function() {
+		$('.info_btn_m img').toggleClass("rotate");
 	});
 
 	/*
